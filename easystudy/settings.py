42d1f16581980cf9e8684fa5529ac8b9f2a96fd7
John <bizzyjay933@gmail.com>
2024-03-08 19:37:37 +0100

    chathub

--- easystudy/settings.py
+++ easystudy/settings.py
@@ -50,6 +50,8 @@ AUTH_USER_MODEL = 'base.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     "corsheaders.middleware.CorsMiddleware",
 
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -136,8 +138,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = 'static/'
-# MEDIA_URL = '/images/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Emailing settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -180,11 +191,11 @@ PASSWORD_RESET_TIMEOUT = 14400
 
 
 
-# STATICFILES_DIRS = [BASE_DIR/'static',]
+STATICFILES_DIRS = [BASE_DIR/'static',]
 
 
 # for user uploaded content
-# MEDIA_ROOT = BASE_DIR / 'static/images' 
+MEDIA_ROOT = BASE_DIR / 'static/images' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
